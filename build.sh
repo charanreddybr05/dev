27ceaebe293f6ede808f02e2f590c71c5981e018
charanreddybr05 <charanreddy.br.05@gmail.com>
2016-07-19 15:12:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "Hi"
 cp content/target/acs-aem-samples-content-1.0.0-SNAPSHOT.zip /opt/backups
-curl -u admin:"admin" -F name=acs-aem-samples-content -F file=@/opt/backupsacs-aem-samples-content-1.0.0-SNAPSHOT.zip http://192.168.99.100:4502/crx/packmgr/service.jsp?cmd=upload --verbose
+curl -u admin:"admin" -F name=acs-aem-samples-content -F file=@/opt/backups/acs-aem-samples-content-1.0.0-SNAPSHOT.zip http://192.168.99.100:4502/crx/packmgr/service.jsp?cmd=upload --verbose
 curl -u admin:"admin" http://192.168.99.100:4502/crx/packmgr/service.jsp?cmd=inst\&name=acs-aem-samples-content
